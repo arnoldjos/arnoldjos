e6a046fd245548e31e619f9aaccbf29c0d8ec0d1
Arnold Joseph A. Fudolig <arnold.anaviso@gmail.com>
2019-11-28 10:36:34 +0800

    Test nginx

--- ssl_renew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/docker-compose -f /home/arnoldjos/apps/arnoldjos/docker-compose.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f /home/arnoldjos/apps/arnoldjos/docker-compose.yml kill -s SIGHUP proxy
